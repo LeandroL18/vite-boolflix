dae9938280dd5c15f831e47afa588ce6d09c8eab
LeandroL18 <leandrolipani6@gmail.com>
2024-01-12 12:56:27 +0100

    feat: complete structure

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,26 @@
+<script>
+import {store} from '../store'
+
+import CardList from './CardList.vue';
+
+export default{
+    name:'AppSearch',
+    components:{
+        CardList
+    },
+    data(){
+        return{
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <CardList/>
+</template>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+</style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,25 @@
+<script>
+import {store} from '../store'
+import ProductCard from './ProductCard.vue'
+
+export default{
+    name:'CardList',
+    components:{
+        ProductCard,
+    },
+    data(){
+        return{
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <ProductCard/>
+</template>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+</style>
