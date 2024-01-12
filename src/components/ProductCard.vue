e9b3fb7b0d8c95717a2d8ff3b76a51ddc90d5609
LeandroL18 <leandrolipani6@gmail.com>
2024-01-12 15:25:08 +0100

    fix: trasformazione della stringa lingua in bandiera

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -3,6 +3,24 @@
         name:'ProductCard',
         props:{
             info: Object
+        },
+        data(){
+            return{
+                i: null,
+                flags:[
+                    {
+                        lang:"en",
+                        src:"https://www.iolecal.it/blog/posts/post088/English.jpg",
+                    },
+
+                    {
+                        lang:"it",
+                        src:"public/bandierina_italia.jpg"
+                    }
+
+                    
+                ]
+            }
         }
     }
 </script>
@@ -13,7 +31,7 @@
         <img src="" alt="img">
         <h3>{{ info.title }}</h3>
         <h4>{{ info.original_title }}</h4>
-        <h5>{{ info.original_language }}</h5>
+            <img v-for="(flag,i) in flags" :src="flags[i].lang === info.original_language ? flags[i].src : ''" :alt="flags[i].lang === info.original_language ? flags[i].lang : ''">
         <h5>{{ info.vote_average }}</h5>
 
     </div>
